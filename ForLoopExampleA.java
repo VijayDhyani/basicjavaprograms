53ff479d293dccb9c7db9501fc30e4b247c95fc4
VijayDhyani <37833958+VijayDhyani@users.noreply.github.com>
2018-09-14 07:19:32 +0530

    Add files via upload

---
+++ ForLoopExampleA.java
@@ -0,0 +1,12 @@
+package forloop;
+
+public class ForLoopExampleA {
+    public static void main(String[] args) {
+
+
+        int c;
+        for (c = 0; c <= 10; c++) {
+            System.out.println("Hare Krishna");
+        }
+    }
+}
